1a33494968ad867053609bb93a9c077ae6e46178
Dr. Abder-Rahman Ali <abder@cs.stir.ac.uk>
2024-06-18 17:38:38 -0300

    Update k_means.py

--- k_means.py
+++ k_means.py
@@ -1,7 +1,7 @@
 '''
 @author: Abder-Rahman Ali, PhD
 @email: aali25@mgh.harvard.edu
-@date: 2023
+@date: 2024
 '''
 
 import os
@@ -15,7 +15,6 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
 import pandas as pd
 
-# Cosine K-means class
 class CosineKMeans(KMeans):
     def _transform(self, X):
         return pairwise_distances(X, self.cluster_centers_, metric='cosine')
@@ -35,7 +34,6 @@ class ImageDataset(Dataset):
             image = self.transform(image)
         return image, self.image_files[idx]
 
-# Define the transform to convert images to tensor and normalize
 transform = transforms.Compose([transforms.Resize((512, 512)), 
                                 transforms.Grayscale(num_output_channels=3),
                                 transforms.ToTensor(), 
@@ -48,10 +46,8 @@ dataloader = DataLoader(dataset, batch_size=len(dataset))
 
 images, image_names = next(iter(dataloader))
 
-# Reshape the images to be 2D (they are 3D from the loader)
 images = images.view(images.size(0), -1)
 
-# Convert to numpy array for use with KMeans
 features = images.numpy()
 
 print("Clustering images...")
@@ -61,16 +57,12 @@ kmeans.fit(features)
 
 labels = kmeans.labels_
 
-# Find the index of the closest image to each cluster center
 centroids = [np.argmin(pairwise_distances(features, center.reshape(1, -1), metric='cosine')) for center in kmeans.cluster_centers_]
 
-# Get the distances to the nearest cluster center for each point
 distances = kmeans.transform(features)
 
-# For each point, get the distance to its cluster centroid
 centroid_distances = distances[np.arange(len(distances)), kmeans.labels_]
 
-# Create a dictionary where the keys are the cluster labels and the values are lists of image names
 clusters = {i: [] for i in range(5)}
 centroid_flags = {i: [] for i in range(5)}
 outlier_flags = {i: [] for i in range(5)}
@@ -79,13 +71,11 @@ for i, label in enumerate(labels):
     centroid_flags[label].append(i in centroids)
     outlier_flags[label].append(centroid_distances[i])
 
-# Identify the top 5 outliers in each cluster
 for label in outlier_flags:
     outlier_flags[label] = np.argsort(outlier_flags[label])[-5:]
 
 print("Saving images to CSV files...")
 
-# Save the image names for each cluster to a CSV file
 for label, image_names in clusters.items():
     df = pd.DataFrame({
         'image_name': image_names,
